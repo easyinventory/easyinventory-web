34e272d08f52fc60d4f668d560d9ff49db262b6d
sbrock928 <97187437+sbrock928@users.noreply.github.com>
2026-03-16 12:35:06 -0400

    update: App.tsx wraps with AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./auth/AuthContext";
 import AppLayout from "./components/layout/AppLayout";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -14,24 +15,26 @@ import NotFoundPage from "./pages/NotFoundPage";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/login" element={<LoginPage />} />
+      <AuthProvider>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* App routes (will be protected in PR-11) */}
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/store-layout" element={<StoreLayoutPage />} />
-          <Route path="/analytics" element={<AnalyticsPage />} />
-          <Route path="/suppliers" element={<SuppliersPage />} />
-          <Route path="/org-settings" element={<OrgSettingsPage />} />
-        </Route>
+          {/* App routes (will be protected in PR-11) */}
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/inventory" element={<InventoryPage />} />
+            <Route path="/store-layout" element={<StoreLayoutPage />} />
+            <Route path="/analytics" element={<AnalyticsPage />} />
+            <Route path="/suppliers" element={<SuppliersPage />} />
+            <Route path="/org-settings" element={<OrgSettingsPage />} />
+          </Route>
 
-        {/* Catch-all */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+          {/* Catch-all */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
