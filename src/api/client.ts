cf0f5da7e31340759168e60e6fc6f33dc19ccc39
sbrock928 <97187437+sbrock928@users.noreply.github.com>
2026-03-16 12:13:14 -0400

    add: API client utility

---
+++ src/api/client.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// PR-09 will add an interceptor here to attach the JWT:
+// apiClient.interceptors.request.use((config) => {
+//   const token = getToken();
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+export default apiClient;
