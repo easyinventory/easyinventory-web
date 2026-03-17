be49d179c159bd76ab6b03632020dd4a38f4946d
sbrock928 <97187437+sbrock928@users.noreply.github.com>
2026-03-17 16:59:40 -0400

    removed unused component

--- src/components/suppliers/index.ts
+++ src/components/suppliers/index.ts
@@ -1,4 +1,3 @@
 export { default as SupplierForm } from "./SupplierForm";
-export { default as SupplierRow } from "./SupplierRow";
 export { default as SupplierSearchSelect } from "./SupplierSearchSelect";
 export type { SupplierFormValues } from "./SupplierForm";
