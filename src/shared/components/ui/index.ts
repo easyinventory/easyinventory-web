f342f345c5004bae36c33fe1c191506824952ea0
sbrock928 <97187437+sbrock928@users.noreply.github.com>
2026-03-18 21:32:26 -0400

    feat: add ErrorBoundary component to shared UI library

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,3 +1,4 @@
+export { default as ErrorBoundary } from "./ErrorBoundary";
 export { default as LoadingState } from "./LoadingState";
 export { default as ErrorBanner } from "./ErrorBanner";
 export { default as SuccessBanner } from "./SuccessBanner";
